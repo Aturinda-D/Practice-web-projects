838cbea30f62a219306756b54b6ae939ab357e1a
deidave <atudav15@gmail.com>
2025-03-19 12:24:58 +0300

    Login page and first section of home page

---
+++ Simple-banking-system/src/scripts/index.js
@@ -0,0 +1,48 @@
+const loginButton = document.querySelector("[type = 'submit']");
+const logoutButton = document.getElementById("logout");
+const username =document.getElementById("username");
+const password = document.getElementById("password");
+
+const depositButton = document.getElementById("deposit");
+const withdrawButton = document.getElementById("withdraw");
+const depositForm = document.querySelector(".deposit");
+const withdrawForm = document.querySelector(".withdraw");
+
+const checkInputs = () => {
+    if (username.value && password.value){
+        window.location.href = "/Simple-banking-system/src/home/home.html";
+    }
+}
+
+function toggleForm(button) {
+    if (button.id === "deposit"){
+        form = depositForm;
+    } else if (button.id === "withdraw") {
+        form = withdrawForm;
+    }
+    switch (form.style.display) {
+        case "none":
+            form.style.display = "block";
+            break;
+        default:
+            form.style.display = "none";
+            break;
+    }
+}
+
+if (window.location.pathname === "/Simple-banking-system/src/index.html"){
+    loginButton.addEventListener('click', (e)=>{
+        e.preventDefault();
+        checkInputs();
+    });
+} else if (window.location.pathname === "/Simple-banking-system/src/home/home.html"){
+    logoutButton.addEventListener('click', ()=>{
+        window.location.href = "/Simple-banking-system/src/index.html";
+    });
+    depositButton.addEventListener('click', (e)=>{
+        toggleForm(e.target);
+    });
+    withdrawButton.addEventListener('click', (e)=>{
+        toggleForm(e.target);
+    });
+}
