ec07111f7ff86e6a8ff1dda369bfb81e51d5d4ca
deidave <atudav15@gmail.com>
2025-03-20 19:23:42 +0300

    Automatic updates and storage for account balance
    withdraw and deposit functionality

--- Simple-banking-system/src/scripts/index.js
+++ Simple-banking-system/src/scripts/index.js
@@ -8,6 +8,9 @@ const withdrawButton = document.getElementById("withdraw");
 const depositForm = document.querySelector(".deposit");
 const withdrawForm = document.querySelector(".withdraw");
 
+// localStorage.setItem("user-account", JSON.stringify({ balance: 25000 }));
+let userInfo = JSON.parse(localStorage.getItem("user-account"));
+
 const checkInputs = () => {
   if (username.value && password.value) {
     window.location.href = "/Simple-banking-system/src/home/home.html";
@@ -35,6 +38,12 @@ function toggleForm(button) {
   }
 }
 
+function updateBalance() {
+  const userInfo = JSON.parse(localStorage.getItem("user-account"));
+  const balanceDisplay = document.getElementById("account-balance");
+  balanceDisplay.innerHTML = userInfo.balance.toLocaleString();
+}
+
 if (window.location.pathname === "/Simple-banking-system/src/index.html") {
   loginButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -51,5 +60,25 @@ if (window.location.pathname === "/Simple-banking-system/src/index.html") {
   });
   withdrawButton.addEventListener("click", (e) => {
     toggleForm(e.target);
+  });
+  depositForm.querySelector("button").addEventListener("click", (e) => {
+    e.preventDefault();
+    const deposit = document.getElementById("deposit-amount");
+    const amount = parseInt(deposit.value ? deposit.value : 0);
+    userInfo.balance += amount;
+    localStorage.setItem("user-account", JSON.stringify(userInfo));
+    updateBalance();
+    deposit.value = null;
+  });
+  withdrawForm.querySelector("button").addEventListener("click", (e) => {
+    e.preventDefault();
+    const withdraw = document.getElementById("withdraw-amount");
+    const amount = parseInt(withdraw.value ? withdraw.value : 0);
+    if (amount <= userInfo.balance) {
+      userInfo.balance -= amount;
+      localStorage.setItem("user-account", JSON.stringify(userInfo));
+      updateBalance();
+      withdraw.value = null;
+    }
   });
 }
