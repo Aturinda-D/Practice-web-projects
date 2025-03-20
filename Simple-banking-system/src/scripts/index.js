156d2a269cd6e6444128d43d6f3e5bfd39f25482
deidave <atudav15@gmail.com>
2025-03-20 09:28:47 +0300

    Enabled prettier extension for banking system

--- Simple-banking-system/src/scripts/index.js
+++ Simple-banking-system/src/scripts/index.js
@@ -1,6 +1,6 @@
 const loginButton = document.querySelector("[type = 'submit']");
 const logoutButton = document.getElementById("logout");
-const username =document.getElementById("username");
+const username = document.getElementById("username");
 const password = document.getElementById("password");
 
 const depositButton = document.getElementById("deposit");
@@ -9,45 +9,47 @@ const depositForm = document.querySelector(".deposit");
 const withdrawForm = document.querySelector(".withdraw");
 
 const checkInputs = () => {
-    if (username.value && password.value){
-        window.location.href = "/Simple-banking-system/src/home/home.html";
-    }
-}
+  if (username.value && password.value) {
+    window.location.href = "/Simple-banking-system/src/home/home.html";
+  }
+};
 
 function toggleForm(button) {
-    let form;
-    let other;
-    if (button.id === "deposit"){
-        form = depositForm;
-        other = withdrawForm;
-    } else if (button.id === "withdraw") {
-        form = withdrawForm;
-        other = depositForm;
-    }
-    switch (form.style.display) {
-        case "none":
-            form.style.display = "block";
-            other.style.display = 'none';
-            break;
-        default:
-            form.style.display = "none";
-            break;
-    }
+  let form;
+  let other;
+  if (button.id === "deposit") {
+    form = depositForm;
+    other = withdrawForm;
+  } else if (button.id === "withdraw") {
+    form = withdrawForm;
+    other = depositForm;
+  }
+  switch (form.style.display) {
+    case "none":
+      form.style.display = "block";
+      other.style.display = "none";
+      break;
+    default:
+      form.style.display = "none";
+      break;
+  }
 }
 
-if (window.location.pathname === "/Simple-banking-system/src/index.html"){
-    loginButton.addEventListener('click', (e)=>{
-        e.preventDefault();
-        checkInputs();
-    });
-} else if (window.location.pathname === "/Simple-banking-system/src/home/home.html"){
-    logoutButton.addEventListener('click', ()=>{
-        window.location.href = "/Simple-banking-system/src/index.html";
-    });
-    depositButton.addEventListener('click', (e)=>{
-        toggleForm(e.target);
-    });
-    withdrawButton.addEventListener('click', (e)=>{
-        toggleForm(e.target);
-    });
+if (window.location.pathname === "/Simple-banking-system/src/index.html") {
+  loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    checkInputs();
+  });
+} else if (
+  window.location.pathname === "/Simple-banking-system/src/home/home.html"
+) {
+  logoutButton.addEventListener("click", () => {
+    window.location.href = "/Simple-banking-system/src/index.html";
+  });
+  depositButton.addEventListener("click", (e) => {
+    toggleForm(e.target);
+  });
+  withdrawButton.addEventListener("click", (e) => {
+    toggleForm(e.target);
+  });
 }
